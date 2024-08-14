dd1e35c1265fb18ab5fd9194c96f71bb8b221808
James McArthur <77163783+jamezmca@users.noreply.github.com>
2024-08-15 00:33:19 +1200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -9,7 +9,7 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 export default function Hero() {
     return (
         <div className='py-4 md:py-10 flex flex-col gap-8 sm:gap-10'>
-            <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'>Broodl</span> helps you track you <span className='textGradient'>daily</span> mood!</h1>
+            <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}><span className='textGradient'>Broodl</span> helps you track your <span className='textGradient'>daily</span> mood!</h1>
             <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Create your mood record and see how you feel on <span className='font-semibold'>every day of every year.</span></p>
             <CallToAction />
             <Calendar demo />
